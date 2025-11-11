98a7130f3a714eeb23128273caf7cf7bdd0ab037
Khaled Elmasry <69255060+KhaledAhmedElmasry@users.noreply.github.com>
2025-11-11 09:27:28 +0200

    Instructor header

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,38 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
-
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
-
+#include <iostream>
+class Instructor:public Person {
+private:
+    string department;
+    int experienceYears;
+public:
+    Instructor() {
+        department ="unknown";
+        experienceYears = 0;
+    }
+    Instructor(string d,int ey) {
+        department = d;
+        experienceYears =ey;
+    }
+    ~Instructor() {
+    }
+    void setDepartment(string d) {
+        department = d;
+    }
+    void setExperienceYears(int ey) {
+        experienceYears = ey;
+    }
+    string getDepartment() {
+        return department;
+    }
+    int getExperienceYears() {
+        return experienceYears;
+    }
+    void display() {
+        cout << "department: " << department << endl;
+        cout << "experience years: " << experienceYears;
+    }
+};
 #endif
