e9f405f9764d6912d6ec7442bd4f17f33b280df8
Khaled Elmasry <69255060+KhaledAhmedElmasry@users.noreply.github.com>
2025-11-11 09:08:16 +0200

    person header

--- Person.h
+++ Person.h
@@ -1,20 +1,40 @@
 #ifndef PERSON_H
 #define PERSON_H
-
 #include <string>
+#include <iostream>
 using namespace std;
+class Person {
+private:
+    string name;
+    int id ;
+public:
+    Person() {
+        name = "noname";
+        id = 0;
+    }
+    Person(string n,int i) {
+        name = n;
+        id = i;
+    }
+    ~Person() {
+    }
+    void setName(string n) {
+         name = n;
+    }
+    void setId(int i) {
+        id = i;
+    }
+    string getName() {
+        return name;
+    }
+    int getId() {
+        return id;
+    }
+    void display() {
+        cout << "name: " << name << endl;
+        cout << "id: " << id;
+    }
 
-//#write Person class here
 
-
-
-
-
-
-
-
-
-
-
-
+};
 #endif
