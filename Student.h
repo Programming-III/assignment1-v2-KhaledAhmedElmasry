efa9d424aea6ae0119aa5f52359638caf4f2c9f9
Khaled Elmasry <69255060+KhaledAhmedElmasry@users.noreply.github.com>
2025-11-11 09:18:14 +0200

    Student header
    Added Student class with year level and major attributes.

--- Student.h
+++ Student.h
@@ -1,11 +1,43 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
 #include "Person.h"
 #include <string>
-#using namespace std;
-//#write student class here
+using namespace std;
+#include <iostream>
+class Student : public Person{
+private:
+    int yearLevel;
+    string major;
+public:
+    Student() {
+        yearLevel = 0;
+        major ="unknown";
+    }
+    Student(int yl, string m) {
+        yearLevel = yl;
+        major = m;
+    }
+    ~Student() {
+    }
+    void setYearLevel(int yl) {
+        yearLevel = yl;
+    }
 
+    void setmajor(string m) {
+        major = m;
+    }
+    int getYearLevel() {
+        return yearLevel;
+    }
+    string getMajor() {
+        return major;
+    }
+    void display() {
+        cout << "year level: " << yearLevel << endl;
+        cout << "major: " << major;
+    }
+
+};
 
 
 
